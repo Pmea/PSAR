62ef33e5d45d487f4bcff0dd098e5ffa62368031
Pmea <kurugan04@hotmail.fr>
2015-03-20 11:46:34 +0100

    reception du lightSensor

---
+++ Src/Pduino/PhotoCell_Pduino/PhotoCell_Pduino.ino
@@ -0,0 +1,43 @@
+int photocellPin = 0;     // pin pour connecter la photocell avec resitance (pulldown de 2K) pour les donnees a0
+int photocellReading;     // the analog reading from the sensor divider
+int LEDpin = 11;          // connect LED to pin 11 
+int LEDbrightness;        
+
+int valMax;    //pour adapter le systeme a l'environement
+int valMin;
+
+void setup(void) {
+  Serial.begin(9600);   
+  valMax=0;
+  valMin= (1 << 15) -1 ; // max int 
+}
+ 
+void loop(void) {
+  photocellReading = analogRead(photocellPin);    
+  
+  if(valMin > photocellReading){
+    //Serial.print("New Min = ");
+    //Serial.println(photocellReading);
+    valMin= photocellReading;
+  }
+  if(valMax < photocellReading){
+    //Serial.print("New Max = ");
+    //Serial.println(photocellReading);
+    valMax= photocellReading;
+  }
+  
+  photocellReading= map(photocellReading, valMin, valMax, 0, 100);
+  //Serial.print("Analog reading = ");
+  //Serial.println(photocellReading);     // the raw analog reading
+ 
+  LEDbrightness = map(photocellReading, 0, 100, 0, 255);
+  //Serial.print("\tLed brightness = ");
+  //Serial.println(LEDbrightness);     // led data
+ 
+
+  analogWrite(LEDpin, LEDbrightness);
+  
+  Serial.write(photocellReading);
+ 
+  delay(100);
+}
