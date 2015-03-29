65db26b93dbe877917cc30c648d5db9ba6732a44
Pmea <kurugan04@hotmail.fr>
2015-03-29 13:28:20 +0200

    ajout moyenne pour caspteur et debut analyse melodie

--- Src/Pduino/PhotoCell_Pduino/PhotoCell_Pduino.ino
+++ Src/Pduino/PhotoCell_Pduino/PhotoCell_Pduino.ino
@@ -3,49 +3,72 @@ int photocellReading;     // the analog reading from the sensor divider
 int LEDpin = 11;          // connect LED to pin 11 
 int LEDbrightness;        
 
-int valDelay=10;      // en miliseconde * 10
+int valDelay=100;      // en miliseconde
 
 int valMax;    //pour adapter le systeme a l'environement
 int valMin;
 
+int moy;
+int lastVal;
+long nbEchant=0;
+
 void setup(void) {
   Serial.begin(9600);   
-  valMax=0;
-  valMin= (1 << 15) -1 ; // max int 
   
+  moy= analogRead(photocellPin);
+  valMax= moy + moy * ( 20 / 100 );
+  valMin= moy - moy * ( 20 / 100 );
+  lastVal= moy;
 }
  
 void loop(void) {
   Serial.flush();
   if(Serial.available()){
-    valDelay= Serial.read();
+    valDelay= Serial.parseInt();  // ou sinon avec OSC a voir 
   }
   
+  
+  lastVal= photocellReading;
   photocellReading = analogRead(photocellPin);    
+  nbEchant++;  
+  moy= moy +  ((float)photocellReading /(float)nbEchant);
   
   if(valMin > photocellReading){
     //Serial.print("New Min = ");
     //Serial.println(photocellReading);
     valMin= photocellReading;
   }
+  
   if(valMax < photocellReading){
     //Serial.print("New Max = ");
     //Serial.println(photocellReading);
     valMax= photocellReading;
   }
   
-  photocellReading= map(photocellReading, valMin, valMax, 0, 100);
+  LEDbrightness= map(photocellReading, valMin, valMax, 0, 255);
   //Serial.print("Analog reading = ");
   //Serial.println(photocellReading);     // the raw analog reading
  
-  LEDbrightness = map(photocellReading, 0, 100, 0, 255);
   //Serial.print("\tLed brightness = ");
   //Serial.println(LEDbrightness);     // led data
  
 
   analogWrite(LEDpin, LEDbrightness);
-  Serial.print('l');
-  Serial.write(photocellReading);
+  
+  Serial.print('valphotocell: ');
+  Serial.println(photocellReading);
+  
+  Serial.print('lastphotocell:');
+  Serial.println(lastVal);
  
-  delay(valDelay*10);
+  delay(valDelay);
+}
+
+void periodique(int lastVal, int lastMoy){
+  
+  nbEchant=0;
+  lastVal= lastVal;   // en theorie inutile
+  moy= lastMoy;      // en theorie inutile
+  valMax= moy + moy * ( 20 / 100 );
+  valMin= moy - moy * ( 20 / 100 );
 }
