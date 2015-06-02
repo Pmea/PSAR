0f122e977949adbc2cc390bd3a92c647acc80f2d
Pmea <mahe.pierre@live.fr>
2015-06-02 20:46:56 +0200

    documentation pour la partie arduino

--- Src/Pduino/PhotoCell_Pduino/PhotoCell_Pduino.ino
+++ Src/Pduino/PhotoCell_Pduino/PhotoCell_Pduino.ino
@@ -1,15 +1,19 @@
-// rapport valeur moyenne changement d'idee
+//TODO
+// Il serait interessant d'ajouter un Watchdog pour redemarer la partie arduino
+// si il y a un bug quelque part
+// pour cela il existe de nombreuse bibliotheque (les liens sont disponible sur le site officiel arduino) 
 
 #include <SimpleTimer.h>
 #include "DHT.h"
 
-#define DHTPIN 2        // what pin we're connected to
-#define DHTTYPE DHT11   // DHT 11 
-#define VOYANTPIN 13
+#define DHTPIN 2        // le pin ou est connecter le module DHT11
+#define DHTTYPE DHT11   // DHT 11, le type de module pour la temperature et l'humidité
+#define VOYANTPIN 13    // le pin pour le led clignotante, montrant le bon fonctionnement de la partie arduino
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int photoCellPin = A7;     // pin pour connecter la photocell avec resitance (pulldown de 2K) pour les donnees a0
+int photoCellPin = A7;     // pin pour connecter la photocell avec resitance (pulldown de 2K) pour les donnees a7
 int photoCellReading;    
 int temperatureCellReading;
 int humidityCellReading;
@@ -24,6 +28,7 @@ int lastValPhoto=0;
 int lastValTemp=0;
 int lastValHumi=0;
 
+// timer qui va commender l'allumage periodique de la voyant de fonctionnement
 SimpleTimer timer;
 
 char buff [64];
@@ -35,13 +40,13 @@ void setup(void) {
   dht.begin();
   
   Serial.begin(9600);  
-  timer.setInterval(1000, periodique);
+  timer.setInterval(1000, periodique);    // preparation du timer
  
   lastValPhoto=0;
- lastValTemp=0;
- lastValHumi=0;
+  lastValTemp=0;
+  lastValHumi=0;
  
- delay(500);
+  delay(250);      // delai de sureté pour laisser le temps au capteur DHT de d'initialiser (sinon risque de fausse valeur) 
   photoCellReading= analogRead(photoCellPin);
   temperatureCellReading= dht.readTemperature();
   humidityCellReading= dht.readHumidity();
@@ -56,10 +61,10 @@ void loop(void) {
 
   Serial.flush();
   
-  if(Serial.available()){
+  if(Serial.available()){            // si il y a quelquess choses a lire, sur le canal d'entrée
    char cmd[5+1];   // +1 pour '\0'
    int tmp= recv_cmd(5, cmd);
-   if( strcmp(cmd, "delay") == 0){
+   if( strcmp(cmd, "delay") == 0){      // on verifi que cela correspond bien a la commande changement de delay
      valDelay= tmp;
      
       char buff[64];
@@ -68,15 +73,24 @@ void loop(void) {
     }
   }
   
+  // sauvegarde des dernieres valeurs
   lastValPhoto= photoCellReading;  
   lastValTemp= temperatureCellReading;
   lastValHumi= humidityCellReading;
   
+  // lecture des nouvelles valeurs
   photoCellReading= analogRead(photoCellPin);
   photoCellReading= map(photoCellReading, 0, 1023, 0, 100);
   temperatureCellReading= dht.readTemperature();
   humidityCellReading= dht.readHumidity();
   
+  
+  //TODO pour eviter les erreurs de capteur, il faudrait revifier que la difference entre 
+  // la valeurs sauvegardé et la nouvelle valeur n'est pas plus a plus de 30% de l'ancienne.
+  // de simple if, else a faire
+ 
+  // creation de la commande conforme au protocole et
+  // envoi de la valeur sur le canal de communication
   sprintf(buff, "lgtvl: %d;", photoCellReading);
   Serial.println(buff);
   sprintf(buff, "lgtvr: %d;", photoCellReading -lastValPhoto);
@@ -95,6 +109,8 @@ void loop(void) {
   delay(valDelay);
 }
 
+
+// allumage du voyant pour informer l'etat de fonctionnement du dispositif
 void periodique(void){
   if(on_off){
     digitalWrite( VOYANTPIN, HIGH);
@@ -108,6 +124,8 @@ void periodique(void){
 
 
 
+// traiter la commande recu
+// verification du format et extraction de la valeur
 int recv_cmd(int len_cmd, char* cmd){
   int val;
   
